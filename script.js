f2ac1772197648b0170f57749e1c0f591b0b7c56
Maya Kappen <mayakappen@gmail.com>
2022-06-08 14:59:08 -0600

    Refactor classes

--- script.js
+++ script.js
@@ -1,49 +1,60 @@
 class Player {
-  constructor(fist) {
+  constructor() {
     this.rock = false
     this.paper = false
     this.scissors = false
-    fist = {
+    this.fist = {
       rock: this.rock,
       paper: this.paper,
       scissors: this.scissors,
     }
-    this.fist = fist
   }
   selectRock() {
-    this.rock = true
-    this.paper = false
-    this.scissors = false
+    this.fist.rock = true
+    this.fist.paper = false
+    this.fist.scissors = false
+    return this.fist
   }
   selectPaper() {
-    this.paper = true
-    this.rock = false
-    this.scissors = false
+    this.fist.paper = true
+    this.fist.rock = false
+    this.fist.scissors = false
+    return this.fist
   }
   selectScissors() {
-    this.scissors = true
-    this.paper = false
-    this.rock = false
+    this.fist.scissors = true
+    this.fist.paper = false
+    this.fist.rock = false
+    return this.fist
   }
 };
 
 
 
 class Game {
-  constructor(user) {
-    var user = new Player
-    var computer = new Player
-    var input = {}
-    this.userInput = user.fist
-    this.computerInput = [input]
+  constructor() {
+    var player1 = new Player
+    this.userInput = player1.fist
   }
   randomFist() {
+    var computer = new Player
     var input = this.computerInput
       var options = ['rock', 'paper', 'scissors']
       for (var i = 0; i < options.length ; i++) {
         var randFist = Math.floor(Math.random() * options.length)
         input = randFist
-        return options[input]
+        if (options[input] === 'rock') {
+          computer.selectRock();
+
+        }
+        else if (options[input] === 'paper') {
+          computer.selectPaper();
+        }
+        else if (options[input] === 'scissors') {
+          computer.selectScissors()
+        }
+        return computer.fist
+
   }
-}
+  }
 }
