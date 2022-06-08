e29e266f6bc2cf36e299f407577edc9a6cce05ee
Maya Kappen <mayakappen@gmail.com>
2022-06-08 14:16:51 -0600

    Add Game class

--- script.js
+++ script.js
@@ -3,18 +3,17 @@ class Player {
     this.rock = false
     this.paper = false
     this.scissors = false
-
     fist = {
       rock: this.rock,
       paper: this.paper,
       scissors: this.scissors,
     }
-    }
+    this.fist = fist
+  }
   selectRock() {
     this.rock = true
     this.paper = false
     this.scissors = false
-
   }
   selectPaper() {
     this.paper = true
@@ -26,4 +25,25 @@ class Player {
     this.paper = false
     this.rock = false
   }
+};
+
+
+
+class Game {
+  constructor(user) {
+    var user = new Player
+    var computer = new Player
+    var input = {}
+    this.userInput = user.fist
+    this.computerInput = [input]
+  }
+  randomFist() {
+    var input = this.computerInput
+      var options = ['rock', 'paper', 'scissors']
+      for (var i = 0; i < options.length ; i++) {
+        var randFist = Math.floor(Math.random() * options.length)
+        input = randFist
+        return options[input]
+  }
+}
 }
