2ea904f831c5217fcf96204792e8154af0f2de95
Maya Kappen <mayakappen@gmail.com>
2022-06-11 16:36:22 -0600

    Create files for Player and Game classes

---
+++ Player.js
@@ -0,0 +1,28 @@
+class Player {
+  constructor() {
+    this.wins = 0
+    this.rock = false
+    this.paper = false
+    this.scissors = false
+    this.fist = {
+      rock: this.rock,
+      paper: this.paper,
+      scissors: this.scissors,
+    }
+  }
+  selectRock() {
+    this.fist.rock = true
+    this.fist.paper = false
+    this.fist.scissors = false
+  }
+  selectPaper() {
+    this.fist.paper = true
+    this.fist.rock = false
+    this.fist.scissors = false
+  }
+  selectScissors() {
+    this.fist.scissors = true
+    this.fist.paper = false
+    this.fist.rock = false
+}
+};
--- script.js
+++ script.js
@@ -1,78 +1,15 @@
+
 var playButton = document.getElementById('play')
 var rockButton = document.getElementById('select-rock')
 var paperButton = document.getElementById('select-paper')
 var scissorsButton = document.getElementById('select-scissors')
+var playerHand = document.getElementById('player-hand')
+var computerHand = document.getElementById('computer-hand')
 var area = document.querySelector('.result')
 
-class Player {
-  constructor() {
-    this.wins = 0
-    this.rock = false
-    this.paper = false
-    this.scissors = false
-    this.fist = {
-      rock: this.rock,
-      paper: this.paper,
-      scissors: this.scissors,
-    }
-  }
-  selectRock() {
-    this.fist.rock = true
-    this.fist.paper = false
-    this.fist.scissors = false
-  }
-  selectPaper() {
-    this.fist.paper = true
-    this.fist.rock = false
-    this.fist.scissors = false
-  }
-  selectScissors() {
-    this.fist.scissors = true
-    this.fist.paper = false
-    this.fist.rock = false
-}
-};
 
 var player1 = new Player
 var computer = new Player
-
-class Game {
-  constructor() {
-  }
-  randomFist() {
-    var input = this.computerInput
-      var options = ['rock', 'paper', 'scissors']
-      for (var i = 0; i < options.length ; i++) {
-        var randFist = Math.floor(Math.random() * options.length)
-        input = randFist
-        if (options[input] === 'rock') {
-          computer.selectRock();
-        }
-          else if (options[input] === 'rock' && player1.fist.paper === true) {
-          computer.selectRock();
-        }
-        else if (options[input] === 'paper') {
-          computer.selectPaper();
-
-        }
-        else if (options[input] === 'scissors') {
-          computer.selectScissors()
-        }
-
-  }
-  }
-  shoot() {
-    if ((computer.fist.rock === true && player1.fist.rock === true) || (computer.fist.scissors === true && player1.fist.scissors === true) || (computer.fist.paper === true && player1.fist.paper === true)) {
-      return 'It\'s a Draw!'
-  } else if ((computer.fist.rock === true && player1.fist.scissors === true) || (computer.fist.scissors === true && player1.fist.paper === true) || (computer.fist.paper === true && player1.fist.rock === true)) {
-      computer.wins++
-      return 'You Lose!'
-  } else if ((player1.fist.rock === true && computer.fist.scissors === true) || (player1.fist.scissors === true && computer.fist.paper === true) || (player1.fist.paper === true && computer.fist.rock === true)) {
-      player1.wins++
-      return 'You Win!'
-  }
-  }
-}
 
 
 
@@ -99,6 +36,6 @@ function newGame() {
 }
 
 playButton.addEventListener('click', newGame)
-rockButton.addEventListener('click', selectRock)
+rockButton.addEventListener('click', player1.selectRock())
 paperButton.addEventListener('click', selectPaper)
 scissorsButton.addEventListener('click', selectScissors)
