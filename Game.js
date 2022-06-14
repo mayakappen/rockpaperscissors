aa2a118dd8e27b049edab6139ea4930c7a2c3851
Maya Kappen <mayakappen@gmail.com>
2022-06-14 11:22:05 -0600

    Add shootExtra method and randomFistExtra method

--- Game.js
+++ Game.js
@@ -6,7 +6,7 @@ class Game {
     this.player1 = new Player('human', '😂')
     this.computer = new Player('computer', '🤖')
   }
-  randomFist() {
+  randomFistNormal() {
     var input = this.computerInput
     var options = ['rock', 'paper', 'scissors']
     for (var i = 0; i < options.length; i++) {
@@ -26,6 +26,34 @@ class Game {
         }
       }
   }
+  randomFistExtra() {
+    var input = this.computerInput
+    var options = ['rock', 'paper', 'scissors', 'peace', 'hang']
+    for (var i = 0; i < options.length; i++) {
+      var randFist = Math.floor(Math.random() * options.length)
+      input = randFist
+        if (options[input] === 'rock') {
+          this.computer.takeTurn('rock')
+          computerHand.innerHTML = computerImages[0]
+        }
+        else if (options[input] === 'paper') {
+          this.computer.takeTurn('paper')
+          computerHand.innerHTML = computerImages[2]
+        }
+        else if (options[input] === 'scissors') {
+        this.computer.takeTurn('scissors')
+        computerHand.innerHTML = computerImages[1]
+        }
+        else if (options[input] === 'peace') {
+        this.computer.takeTurn('peace')
+        computerHand.innerHTML = computerImages[3]
+        }
+        else if (options[input] === 'hang') {
+        this.computer.takeTurn('hang')
+        computerHand.innerHTML = computerImages[4]
+        }
+      }
+  }
   shootNormal() {
     if (this.player1.rock === false && this.player1.paper === false && this.player1.scissors === false) {
       this.computer.wins++
@@ -40,6 +68,20 @@ class Game {
       this.player1.wins++
       return 'You Win!'
   }
+  shootExtra() {
+    if (this.player1.rock === false && this.player1.paper === false && this.player1.scissors === false && this.player1.hang === false && this.player1.peace === false) {
+      this.computer.wins++
+      playerHand.innerHTML = playerImages[3]
+      return 'Select an Option!'
+  } else if ((this.computer.rock === true && this.player1.rock === true) || (this.computer.scissors === true && this.player1.scissors === true) || (this.computer.paper === true && this.player1.paper === true) || (this.computer.peace=== true && this.player1.peace=== true) || (this.computer.hang === true && this.player1.hang === true)) {
+      return 'It\'s a Draw!'
+  } else if (((this.computer.rock === true && (this.player1.scissors === true || this.player1.hang === true)) || (this.computer.scissors === true && (this.player1.paper === true || this.player1.hang === true)) || (this.computer.paper === true && (this.player1.rock === true || this.player1.peace === true)) || (this.computer.hang === true && (this.player1.paper === true || this.player1.peace === true)) || (this.computer.peace === true && (this.player1.scissors === true || this.player1.rock === true)))  {
+      this.computer.wins++
+      return 'You Lose!'
+  } else if (((this.player1.rock === true && (this.computer.scissors === true || this.computer.hang === true)) || (this.player1.scissors === true && (this.computer.paper === true || this.computer.hang === true)) || (this.this.player1.paper === true && (this.computer.rock === true || this.computer.peace === true)) || (this.player1.hang === true && (this.computer.paper === true || this.computer.peace === true)) || (this.player1.peace === true && (this.computer.scissors === true || this.computer.rock === true)))  {
+      this.player1.wins++
+      return 'You Win!'
+  }
 }
   newGameNormal() {
     this.randomFist();
@@ -50,5 +92,4 @@ class Game {
     window.setTimeout(switchViews, 2500)
     this.player1.selectEmpty();
   }
-
 }
